f0bf5812bb5a21debb34f89ee4b14011b8f709b2
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-10 18:42:28 +0300

    Create DocumentorException.php

---
+++ src/DocumentorException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package evas-php\evas-documentor
+ */
+namespace Evas\Documentor;
+
+/**
+ * Исключение документора.
+ * @author Egor Vasyakin <egor@evas-php.com>
+ * @since 17 Jul 2020
+ */
+class DocumentorException extends \Exception
+{}
