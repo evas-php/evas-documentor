254706132e602756c4036988f8da2b7f418b4d91
Egor Vasyakin <e.vasyakin@itevas.ru>
2021-08-10 18:43:43 +0300

    Create Log.php

---
+++ src/Log.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package evas-php\evas-documentor
+ */
+namespace Evas\Documentor;
+
+class Log
+{
+    public static $level = 0;
+
+    public static function tabLine()
+    {
+        // 
+    }
+}
